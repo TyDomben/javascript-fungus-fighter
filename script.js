49c500a04e112ae1b63dca16537f5cccefc8ac1c
Ty Domben <127000572+TyDomben@users.noreply.github.com>
2023-12-04 21:00:54 -0600

    finishing touches v.2

--- script.js
+++ script.js
@@ -1,24 +1,73 @@
 // State Variables can be declared outside of the onReady
- // 🧠 Remember
-    // - Handle events that ->
-    // - Updates state which is ->
-    // - Rendered to the DOM
+
+// 🧠 Remember
+
+// - Handle events that -> - Updates state which is -> - Rendered to the DOM
 
 let playerAP = 100; // Initial Attack Points for the player
 let fungusHP = 100; // Initial Hit Points for the Freaky Fungus
 
-const apText = document.querySelector('.ap-text');
-const hpText = document.querySelector('.hp-text');
-const apMeter = document.getElementById('ap-meter');
-const hpMeter = document.getElementById('hp-meter');
-const freakyFungus = document.querySelector('.freaky-fungus');
+const apText = document.querySelector(".ap-text");
+const hpText = document.querySelector(".hp-text");
+const apMeter = document.getElementById("ap-meter");
+const hpMeter = document.getElementById("hp-meter");
+const freakyFungus = document.querySelector(".freaky-fungus");
 
+document
+  .querySelector(".arcane-scepter")
+  .addEventListener("click", () => performAttack(12, 14));
+document
+  .querySelector(".entangle")
+  .addEventListener("click", () => performAttack(23, 9));
+document
+  .querySelector(".dragon-blade")
+  .addEventListener("click", () => performAttack(38, 47));
+document
+  .querySelector(".star-fire")
+  .addEventListener("click", () => performAttack(33, 25));
 
-function onReady() {
-    console.log("Ready to go!")
-    
-   
+function performAttack(apCost, hpDamage) {
+    if (playerAP >= apCost) {
+//adjustment for AP and HP
+        playerAP -= apCost;
+        fungusHP -= hpDamage;
+
+  console.log(`Player AP: ${playerAP}, Freaky Fungus HP: ${fungusHP}`);
+
+  // Update the DOM to reflect the new state
+  updateDOM();
+  // Check if the game has been won or lost
+  checkGameStatus();
+}
+
+function updateDOM() {
+    apText.textContent = `${playerAP} AP`;
+    hpText.textContent = `${fungusHP} HP`;
+    apMeter.value = playerAP;
+    hpMeter.value = fungusHP;
 }
 
 
-onReady()
+function onReady() {
+  console.log("Ready to go!");
+}
+
+function checkGameStatus() {
+    if (fungusHP <= 0) {
+        fungusHP = 0; // Ensure HP doesn't go below 0
+        freakyFungus.classList.replace('walk', 'dead');
+        disableAttackButtons();
+        console.log("You win! The Freaky Fungus is defeated.");
+    } else if (playerAP <= 0) {
+        playerAP = 0; // Ensure AP doesn't go below 0
+        freakyFungus.classList.replace('walk', 'jump');
+        disableAttackButtons();
+        console.log("You lose! Humanity is doomed.");
+    }
+}
+
+function disableAttackButtons() {
+    document.querySelectorAll('.attack-btn').forEach(button => button.disabled = true);
+}
+onReady();
+}
